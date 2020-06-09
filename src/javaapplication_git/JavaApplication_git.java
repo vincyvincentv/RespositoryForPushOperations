70d4b0bf381a1cf41ac04d79ce0bb02c70183fb5
vincyvincentv <vincentv@sheridancollege.ca>
2020-06-09 17:40:17 -0400

    TestProject Commit

---
+++ src/javaapplication_git/JavaApplication_git.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication_git;
+
+/**
+ *
+ * @author vincy
+ */
+public class JavaApplication_git {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("push operation");
+    }
+    
+}
